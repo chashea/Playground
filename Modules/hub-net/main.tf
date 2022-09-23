0f979c1de3116e821d8fff7bbd326e18ca068788
Charles Shea <chashea@microsoft.com>
2022-09-23 13:51:32 -0400

    commit changes

--- Modules/hub-net/main.tf
+++ Modules/hub-net/main.tf
@@ -3,6 +3,4 @@ resource "azurerm_virtual_network" "hub-net" {
   location = var.hub-net-location
   address_space = [ "10.0.0.0/16" ]
   resource_group_name = var.hub-net-rg
-  tags = var.hub-net-tags
-
 }
