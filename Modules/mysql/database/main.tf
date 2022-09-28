61ce259f74f5a732cd6dda563a3e464b8656d83b
Charles Shea <chashea@microsoft.com>
2022-09-28 11:54:23 -0400

    commit changes

--- Modules/mysql/database/main.tf
+++ Modules/mysql/database/main.tf
@@ -1,2 +1,40 @@
-resource "azurerm_mysql_server" "mysqldb" {
+resource "azurerm_mysql_server" "mysql-server" {
+    name                    = var.mysqlserver_server_name
+    location                = module.resource-group.azurerm_resource_group.rg.location
+    resource_group_name     = module.resource-group.azurerm_resource_group.rg.name
+    
+    administrator_login          = "mysqladmin"
+    administrator_login_password = "mysqladminpassword"
+
+    sku_name                = var.mysqlserver_sku_name
+    storage_mb              = var.mysqlserver_storage_mb
+    version                 = var.mysqlserver_version
+
+    create_mode             = "replica"
+    ssl_enforcement_enabled = true
+    ssl_minimal_tls_version_enforced = "TLS1_2"
+    public_network_access_enabled = true
+    geo_redundant_backup_enabled = true
+
+    tags = module.resource-group.azurerm_resource_group.rg.tags
+    
 }
+
+## Configuration
+resource "azurerm_mysql_configuration" "mysql-config" {
+    name                = "max_connections"
+    resource_group_name = module.resource-group.azurerm_resource_group.rg.name
+    server_name         = azurerm_mysql_server.mysql-server.name
+    value               = "1000"
+}
+  
+
+## Database
+resource "azurerm_mysql_database" "mysql-db" {
+    name                = "${var.mysqldb_name}"
+    resource_group_name = module.resource-group.azurerm_resource_group.rg.name
+    server_name         = azurerm_mysql_server.mysql-server.name
+    charset             = var.mysqldb_charset
+    collation           = var.mysqldb_collation
+}
+
--- Modules/mysql/database/variables.tf
+++ Modules/mysql/database/variables.tf
@@ -4,10 +4,21 @@ variable "mysqlserver_server_name" {
   description = "this is the namne of the mysql server"
 }
 
-variable "mssql_server_version" {
+variable "mysqlserver_version" {
   type = string
   description = "engine major version"
 }
+
+variable "mysqlserver_sku_name" {
+  type = string
+  description = "this is the sku name of the mysql server"
+}
+
+variable "mysqlserver_storage_mb" {
+  type = number
+  description = "this is the storage size of the mysql server"
+}
+
 
 
 ## Database Variables
@@ -15,5 +26,15 @@ variable "mssql_server_version" {
 variable "mysqldb_name" {
   type = string
   description = "this is the name of the database"
+}
+
+variable "mysqldb_charset" {
+  type = string
+  description = "this is the charset of the database"
+}
+
+variable "mysqldb_collation" {
+  type = string
+  description = "this is the collation of the database"
 }
 
