bc1bd90541b621e21264a9027f1c25ffac5237b4
Charles Shea <chashea@microsoft.com>
2022-09-28 12:33:49 -0400

    commit changes

--- main.tf
+++ main.tf
@@ -4,9 +4,23 @@ module "resource-group" {
   rg-location   = "WestUS"
 }
 
-module "hub-net" {
-  source            = "./Modules/hub-net"
-  hub-net-name      = "vnet-hub-wus"
-  hub-net-location  = module.resource-group.resource_group_location
-  hub-net-rg        = module.resource-group.resource_group_name
-  }
+## module "hub-net" {
+##  source            = "./Modules/hub-net"
+## hub-net-name      = "vnet-hub-wus"
+## hub-net-location  = module.resource-group.resource_group_location
+##  hub-net-rg        = module.resource-group.resource_group_name
+##  }
+
+module "mysql" {
+  source            = "./Modules/mysql"
+  mysqlserver_server_name = "mysqlserver-wus"
+  mysqlserver_version = "5.7"
+  mysqlserver_sku_name = "GP_Gen5_2"
+  mysqlserver_storage_mb = 5120
+  mysqldb_name = "mysqldb"
+  mysqldb_charset = "utf8"
+  mysqldb_collation = "utf8_general_ci"
+  mysqlserver_location = module.resource-group.resource_group_location
+  mysqlserver_rg = module.resource-group.resource_group_name
+}
+  
--- provider.tf
+++ provider.tf
@@ -1,13 +1,13 @@
 terraform {
-    required_version = ">=1.2.9"
+  required_version = ">=1.2.9"
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~>3.23.0"
     }
   }
 }
 
 provider "azurerm" {
-  features{}
+  features {}
 }
