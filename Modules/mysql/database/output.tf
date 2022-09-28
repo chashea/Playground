6086a5e0b4e155a9060d2292de85c7259535392f
Charles Shea <chashea@microsoft.com>
2022-09-28 11:53:49 -0400

    commit changes

--- Modules/mysql/database/output.tf
+++ Modules/mysql/database/output.tf
@@ -1,4 +1,13 @@
-output "mysqlserver_id" {
-  value = azurerm_mssql_server.mysqldb.id
+output "mysqldb-name" {
+  value = azurerm_mysql_database.mysql-db.name
 }
+
+output "mysqlserver-name" {
+  value = azurerm_mysql_server.mysql-server.name
+}
+
+output "mysqlserver-id" {
+  value = azurerm_mysql_server.mysql-server.id
+}
+
 
--- Modules/resource-group/output.tf
+++ Modules/resource-group/output.tf
@@ -9,3 +9,8 @@ output "resource_group_name" {
 output "resource_group_location" {
   value = azurerm_resource_group.rg.location
 }
+
+output "resource_group_tags" {
+  value = azurerm_resource_group.rg.tags
+}
+
