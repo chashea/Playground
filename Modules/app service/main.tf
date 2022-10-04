6fbef5effeb567df1dcf73077c10a82a416e25b0
Charles Shea <chashea@microsoft.com>
2022-10-04 15:18:01 -0400

    commit changes

--- Modules/app service/main.tf
+++ Modules/app service/main.tf
@@ -9,24 +9,58 @@ resource "azurerm_subnet" "ase_subnet" {
 }
 
 # App Service Environment
-
-resource "azurerm_app_service_environment" "ase" {
+resource "azurerm_app_service_environment_v3" "asev3" {
     name                                    = "${var.ase_name}"
     resource_group_name                     = var.rg_name
     location                                = var.ase_location
-    tags                                    = var.ase_tags
     subnet_id                               = var.ase_subnet_id
-    pricing_tier                            = var.ase_pricing_tier
-    front_end_scale_factor                  = var.ase_front_end_scale_factor
-    internal_load_balancing_mode            = var.ase_internal_load_balancing_mode
-    allowed_user_ip_cidrs                   = [var.ase_allowed_user_ip_cidrs]
+    tags                                    = var.ase_tags
     cluster_setting {
         name                                = var.ase_cluster_setting_name
         value                               = var.ase_cluster_setting_value
     }
+
+    cluster_setting {
+        name                                = var.ase_cluster_setting_name
+        value                               = var.ase_cluster_setting_value
+    }
+
+    cluster_setting {
+        name                                = var.ase_cluster_setting_name
+        value                               = var.ase_cluster_setting_value
+    }
+
+}
+# App Service Plan
+resource "azurerm_service_plan" "asp" {
+    name                                    = "${var.asp_name}"
+    resource_group_name                     = var.rg_name
+    location                                = var.rg_location
+    os_type                                 = var.asp_os_type
+    sku_name                                = var.asp_sku_name
+    app_service_environment_id              = var.ase_id
+    tags                                    = var.rg_tags
 }
 
 
-## App Service
+# App Service
 
+resource "azurerm_app_service" "app" {
+    name                                    = "${var.app_name}"
+    resource_group_name                     = var.rg_name
+    location                                = var.rg_location
+    app_service_plan_id                     = var.asp_id    
+    site_config {
+        dotnet_framework_version            = var.site_config_dotnet_framework_version
+        scm_type                            = var.site_config_scm_type
+    }
+    app_settings                            = var.app_app_settings
+    connection_string {
+        name                                = var.app_connection_string_name
+        value                               = var.app_connection_string_value
+        type                                = var.app_connection_string_type
+    }
+    tags                                    = var.rg_tags
+}
+  
 
--- Modules/app service/variables.tf
+++ Modules/app service/variables.tf
@@ -33,10 +33,7 @@ variable "ase_subnet_address_prefix" {
     type = string
     description = "this is the name of the spoke network"  
 }
-variable "ase_subnet_id" {
-  type = string
-  description = "this is the subnet id of the app service"
-}
+
 
 # App Service Environment
 
@@ -56,25 +53,20 @@ variable "ase_tags" {
     description = "this is the tags of the app service environment"
 }
 
+variable "ase_subnet_id" {
+  type = string
+  description = "this is the subnet id of the app service"
+}
+
+
 ## Optional Variables
 variable "ase_pricing_tier" {
     type = string
     description = "this is the pricing tier of the app service environment"
 }
-
-variable "ase_front_end_scale_factor" {
-    type = number
-    description = "this is the front end scale factor of the app service environment"
-}
-
 variable "ase_internal_load_balancing_mode" {
     type = string
     description = "this is the internal load balancing mode of the app service environment"
-}
-
-variable "ase_allowed_user_ip_cidrs" {
-    type = string
-    description = "this is the allowed user ip cidrs of the app service environment"
 }
 
 variable "ase_cluster_setting_name"{
@@ -87,15 +79,20 @@ variable "ase_cluster_setting_value" {
     description = "this is the cluster setting value of the app service environment"
 }
 
+variable "ase_zone_redundant" {
+    type = bool
+    description = "this is the zone redundant of the app service environment"
+}
 
 
 
 
-# App Service 
+
+# App Service Plan
 
 ## Required Variables
 
-variable "app_service_name" {
+variable "asp_name" {
     type = string
     description = "this is the name of the app service"  
 }
@@ -103,5 +100,62 @@ variable "app_service_name" {
 variable "ase_id"{
     type = string
     description = "this is the id of the app service environment"
+}
+
+variable "asp_os_type" {
+    type = string
+    description = "this is the os type of the app service environment"
+}
+
+variable "asp_sku_name" {
+    type = string
+    description = "this is the sku of the app service environment"
+}
+
+
+
+
+# App Service
+
+## Required Variables
+
+variable "app_name" {
+    type = string
+    description = "this is the name of the app service"  
+}
+
+variable "asp_id" {
+    type = string
+    description = "this is the id of the app service environment"
+}
+
+## Optional Variables
+variable "app_app_settings" {
+    type = map(string)
+}
+
+variable "app_connection_string_name" {
+    type = string
+    description = "this is the connection string of the app service"
+}
+
+variable "app_connection_string_value" {
+    type = string
+    description = "this is the connection string of the app service"
+}
+
+variable "app_connection_string_type" {
+    type = string
+    description = "this is the connection string of the app service"
+}
+
+variable "site_config_dotnet_framework_version" {
+    type = string
+    description = "this is the dotnet framework version of the app service"
+}
+
+variable "site_config_scm_type" {
+    type = string
+    description = "this is the scm type of the app service"
 }
 
