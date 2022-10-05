b3dc59cfcbebed4f944266ab4874c2cf0e07096a
Charles Shea <chashea@microsoft.com>
2022-10-05 15:12:38 -0400

    commit changes

--- Modules/resource group/variables.tf
+++ Modules/resource group/variables.tf
@@ -1,14 +1,14 @@
-variable "rg_name" {
+variable "resource_group_name" {
   type = string
   description = "name of your resource group"
 }
 
-variable "rg_location" {
+variable "resource_group_location" {
   type = string
   description = "location of your resources"
 }
 
-variable "rg_tags" {
+variable "resource_group_tags" {
   type = map(string)
   description = "tags for your resources"
 }
