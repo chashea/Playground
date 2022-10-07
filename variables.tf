065879ffd51bfc49f8f6ad7fd3f36431cac23eff
Charles Shea <chashea@microsoft.com>
2022-10-07 10:10:16 -0400

    commit changes

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "resource_group_name" {
+  type        = string
+  description = "The resource group name"
+}
+
+variable "resource_location" {
+  type        = string
+  description = "The resource location, typically matches resource group location"
+}
+
+variable "workload_name" {
+  type        = string
+  description = "The workload or application name"
+}
+
+variable "environment" {
+  type        = string
+  description = "The environment dev / test / prod"
+}
+
+variable "resource_tags" {
+  type        = map(string)
+  description = "The base tags for all the resources"
+}
