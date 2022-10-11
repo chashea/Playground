e20181872af7fb59cc4bac5b07bfd4fd6b41c8a0
Charles Shea <chashea@microsoft.com>
2022-10-11 15:00:50 -0400

    commit changes

--- main.tf
+++ main.tf
@@ -13,6 +13,20 @@ module "hub_net" {
   fw_subnet_name = var.fw_subnet_name
 }
 
+module "firewall_poilcy" {
+  source        = "./Modules/networking/firewall-policy"
+  resource_group_name = var.resource_group_name
+}
 
 
+module "firewall" {
+  source        = "./Modules/networking/firewall"
+  resource_group_name = module.resource_group.resource_group_name
+  resource_group_location = module.resource_group.resource_group_location
+  resource_suffix = local.name_suffix
+  resource_instance = "001"
+  resource_tags = var.resource_tags
+  fw_subnet_id = module.hub_net.fw_subnet_id
+  fw_policy_id = module.firewall_poilcy.fw_policy_id
+}
 
