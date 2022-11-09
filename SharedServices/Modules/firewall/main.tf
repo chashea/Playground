8d09e32ee11f52589c489ee9e18c7a04ed7bea09
Charles Shea <chashea@microsoft.com>
2022-11-09 14:45:09 -0500

    commit changes

--- SharedServices/Modules/firewall/main.tf
+++ SharedServices/Modules/firewall/main.tf
@@ -5,30 +5,30 @@ resource "azurerm_subnet" "fw_subnet" {
   address_prefixes     = ["10.1.0.64/26"]
 }
 
-resource "azurerm_public_ip" "fw_pip"{
-  name                              = "fw-pip-${var.resouce_suffix}-${var.location}"
-  resource_group_name               = var.resource_group_name
-  location                          = var.resource_group_location
-  sku                               = "Standard"
-  allocation_method                 = "Static"
+resource "azurerm_public_ip" "fw_pip" {
+  name                = "fw-pip-${var.resouce_suffix}-${var.location}"
+  resource_group_name = var.resource_group_name
+  location            = var.resource_group_location
+  sku                 = "Standard"
+  allocation_method   = "Static"
 }
 
 resource "azurerm_firewall" "fw" {
-  name                              = "azfw-${var.resource_suffix}-${var.resource_instance}"
-  location                          = var.resource_group_location
-  resource_group_name               = var.resource_group_name
-  sku_name                          = "AZFW_VNet"
-  sku_tier                          = "Premium"
-  zones                             = [ "1,2,3" ]
-  
-    ip_configuration {
-        name                        = "configuration"
-        subnet_id                   = azurerm_subnet.fw_subnet.id
-        public_ip_address_id        = azurerm_public_ip.fw_pip.id
-    }
+  name                = "azfw-${var.resource_suffix}-${var.resource_instance}"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  sku_name            = "AZFW_VNet"
+  sku_tier            = "Premium"
+  zones               = ["1,2,3"]
 
-  tags                                = var.resource_tags
-  firewall_policy_id                  = var.fw_policy_id
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.fw_subnet.id
+    public_ip_address_id = azurerm_public_ip.fw_pip.id
+  }
+
+  tags               = var.resource_tags
+  firewall_policy_id = var.fw_policy_id
 
 
 }
