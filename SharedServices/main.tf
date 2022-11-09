f558e4b453cd618f006c42e8e13d57a43dee02b3
Charles Shea <chashea@microsoft.com>
2022-11-09 13:50:02 -0500

    commit new firewall policy files

--- SharedServices/main.tf
+++ SharedServices/main.tf
@@ -6,11 +6,4 @@ module "resource_group" {
   source              = "./Modules/resource group"
   resource_group_name = var.resource_group_name
 }
-/*
-module "hub_net" {
-  source              = "./Modules/networking/hub-net"
-  resource_group_name = module.resource_group.resource_group_name
-  hub_net_name        = var.hub_net_name
-  subnet_name         = var.subnet_name
-}
-*/
+
--- Workloads/main.tf
+++ Workloads/main.tf
@@ -7,11 +7,5 @@ module "resource_group" {
   resource_group_name = var.resource_group_name
 }
 
-module "waf_policy" {
-  source              = "./Modules/waf-policy"
-  resource_group_name = module.resource_group.resource_group_name
-  resource_suffix     = local.name_suffix
-  resource_instance   = 001
-  resource_tags       = var.resource_tags
-}
+
 
