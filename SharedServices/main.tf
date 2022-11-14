b4a9bd7772eccc82c52646fbc0b9347a6c8958b4
Charles Shea <chashea@microsoft.com>
2022-11-14 12:50:16 -0500

    commit changes

--- SharedServices/main.tf
+++ SharedServices/main.tf
@@ -3,20 +3,21 @@ module "resource_group" {
   resource_group_name = var.resource_group_name
 }
 
-module "firewall_policy" {
+/*module "firewall_policy" {
   source                  = "./Modules/firewall-policy"
   resource_group_name     = var.resource_group_name
   resource_group_location = var.location
   resource_tags           = var.resource_tags
-}
+}*/
 
 module "firewall" {
   source                  = "./Modules/firewall"
   fw_name                 = var.fw_name
   resource_group_name     = var.resource_group_name
   resource_group_location = var.location
-  fw_policy_id            = module.firewall_policy.fw_policy_id
   vnet                    = var.vnet
+  resource_tags           = var.resource_tags
+
 }
 
 
