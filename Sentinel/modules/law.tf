075f30c7eacde0deef8d2eef8cb7251c35045bfe
Charles Shea <chashea@microsoft.com>
2023-03-14 16:42:27 -0400

    adding json

--- Sentinel/modules/law.tf
+++ Sentinel/modules/law.tf
@@ -14,4 +14,11 @@ resource "azurerm_sentinel_log_analytics_workspace_onboarding" "law_onboarding" 
   workspace_name      = azurerm_log_analytics_workspace.law.name
 }
 
-
+// Call the alerts.json template to create the alerts
+resource "azurerm_resource_group_template_deployment" "alerts" {
+  name                = "jsondeploy"
+  resource_group_name = azurerm_resource_group.rg_law.name
+  deployment_mode     = "Incremental"
+  parameters_content  = jsonencode({})
+  template_content    = file("alerts.json")
+}
