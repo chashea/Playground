fb29fcd654fc0331570d5f693a27d62dfd0f8b0b
cshea-msft <chashea@microsoft.com>
2024-05-23 09:46:24 -0400

    updating files

--- Terraform/Vwan/main.tf
+++ Terraform/Vwan/main.tf
@@ -1,36 +1,34 @@
-locals {
-  resource_group_name = "rg-vvwan-eus2-001"
-  location            = "eastus2"
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-vwan-eus2-001"
+  location = "eastus2"
 }
 
-module "vwan" {
-  source                         = "Azure/avm-ptn-virtualwan/azurerm"
-  version                        = "0.4.2"
-  location                       = local.location
-  create_resource_group          = true
-  resource_group_name            = local.resource_group_name
-  allow_branch_to_branch_traffic = true
-  virtual_wan_name               = "vwan-eus2-001"
-  virtual_hubs = {
-    hub1 = {
-      name           = "vhub-eus2-001"
-      address_prefix = "10.1.0.0/16"
-      resource_group = local.resource_group_name
-      location       = local.location
-    }
-  }
+resource "azurerm_virtual_wan" "vwan" {
+  name                = "vwan-eus2-001"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  type                = "Standard"
+}
+
+resource "azurerm_virtual_hub" "vhub" {
+  name                = "vhub-eus2-001"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  virtual_wan_id      = azurerm_virtual_wan.vwan.id
+  address_prefix      = "10.1.0.0/16"
 }
 
 module "azfw" {
   source              = "Azure/avm-res-network-azurefirewall/azurerm"
   version             = ">=0.1.0"
-  resource_group_name = local.resource_group_name
-  location            = local.location
+  enable_telemetry    = var.enable_telemetry
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   name                = "azfw-eus2-001"
   firewall_sku_name   = "AZFW_Hub"
   firewall_sku_tier   = "Premium"
   firewall_virtual_hub = {
-    virtual_hub_id  = module.vwan.virtual_hub_id[0]
+    virtual_hub_id  = azurerm_virtual_hub.vhub.id
     public_ip_count = 3
   }
   firewall_zones     = ["1", "2", "3"]
@@ -40,13 +38,12 @@ module "azfw" {
 module "fwpolicy" {
   source              = "Azure/avm-res-network-firewallpolicy/azurerm"
   version             = ">=0.1.0"
-  resource_group_name = local.resource_group_name
-  location            = local.location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   name                = "fwpolicy-eus2-001"
   firewall_policy_dns = {
     proxy_enabled = true
   }
-  firewall_policy_sku = "Premium"
+  firewall_policy_sku                      = "Premium"
   firewall_policy_threat_intelligence_mode = "Alert"
-
 }
