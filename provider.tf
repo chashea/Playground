d11a630b9fe2e5355a6db7d7d16a31031286428e
cshea15 <chashea@microsoft.com>
2024-05-24 14:16:01 -0400

    updating files

--- provider.tf
+++ provider.tf
@@ -1,14 +1,7 @@
+
 terraform {
   required_version = ">= 1.3.0"
-  backend "azurerm" {
-    resource_group_name = "rg-terraform-state"
-    storage_account_name = "terraformbackendstg"
-    container_name = "terraformbackend"
-    key = "vwan.tfstate"
-  }
-  
   required_providers {
-    # TODO: Ensure all required providers are listed here.
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.71.0"
@@ -17,7 +10,6 @@ terraform {
 }
 
 provider "azurerm" {
-  alias = "vwan"
   features {
     resource_group {
       prevent_deletion_if_contains_resources = true
