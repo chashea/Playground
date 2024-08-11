284113e286eb96edcee27a5f7d27fefe2e673e38
Charles Shea <chashea@microsoft.com>
2024-08-10 21:32:03 -0400

    updating files

--- Terraform/hub&spoke/main.storage.tf
+++ Terraform/hub&spoke/main.storage.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "rg_stg" {
 
 module "storage" {
   source                          = "Azure/avm-res-storage-storageaccount/azurerm"
-  version                         = "0.2.1"
+  version                         = "0.2.2"
   name                            = module.naming.storage_account.name_unique
   resource_group_name             = azurerm_resource_group.rg.name
   location                        = azurerm_resource_group.rg.location
