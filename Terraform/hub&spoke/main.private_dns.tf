2d061795916ca4382e4d350df823cae56bfc8402
Charles Shea <chashea@microsoft.com>
2024-08-21 20:48:00 -0400

    fix dns

--- Terraform/hub&spoke/main.private_dns.tf
+++ Terraform/hub&spoke/main.private_dns.tf
@@ -1,7 +1,7 @@
 module "private_dns_zone" {
   source              = "Azure/avm-res-network-privatednszone/azurerm"
   version             = "0.1.2"
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.rg_hub.name
   domain_name         = "sheadomain.com"
   virtual_network_links = {
     vnetlink0 = {
