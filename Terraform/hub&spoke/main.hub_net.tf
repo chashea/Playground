775de4b57764f14dcb8534d53c86e5f8c6619168
Charles Shea <chashea@microsoft.com>
2024-08-21 20:44:18 -0400

    update all

---
+++ Terraform/hub&spoke/main.hub_net.tf
@@ -0,0 +1,44 @@
+data "azurerm_client_config" "current" {}
+
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = "0.4.1"
+}
+
+resource "azurerm_resource_group" "rg_hub" {
+  name     = "${module.naming.resource_group.name_unique}-vnet"
+  location = "eastus2"
+}
+
+module "hub_vnet" {
+  source              = "Azure/avm-res-network-virtualnetwork/azurerm"
+  version             = "0.4.0"
+  resource_group_name = azurerm_resource_group.rg_hub.name
+  location            = azurerm_resource_group.rg_hub.location
+  name                = "${module.naming.virtual_network.name_unique}-hub"
+  address_space       = ["10.200.0.0/16"]
+  subnets = {
+    subnet0 = {
+      name           = "AzureFirewallSubnet"
+      address_prefix = "10.200.0.0/26"
+    }
+    subnet1 = {
+      name           = "AzureBastionSubnet"
+      address_prefix = "10.200.0.64/26"
+    }
+    subnet2 = {
+      name           = "GatewaySubnet"
+      address_prefix = "10.200.1.0/27"
+    }
+    subnet3 = {
+      name           = "pe-${module.naming.subnet.name_unique}"
+      address_prefix = "10.200.2.0/24"
+    }
+    subnet4 = {
+      name           = "adds-${module.naming.subnet.name_unique}"
+      address_prefix = "10.200.0.128/28"
+    }
+  }
+}
+
+
--- Terraform/hub&spoke/main.storage.tf
+++ Terraform/hub&spoke/main.storage.tf
@@ -3,13 +3,24 @@ resource "azurerm_resource_group" "rg_stg" {
   location = "eastus2"
 }
 
-module "stg" {
+module "file_stg" {
   source                   = "Azure/avm-res-storage-storageaccount/azurerm"
   version                  = "0.2.1"
   name                     = module.naming.storage_account.name_unique
   resource_group_name      = azurerm_resource_group.rg_stg.name
   location                 = azurerm_resource_group.rg_stg.location
+  account_tier             = "Premium"
+  account_replication_type = "ZRS"
+  account_kind = "FileStorage"
+}
+
+module "stg" {
+  source                   = "Azure/avm-res-storage-storageaccount/azurerm"
+  version                  = "0.2.1"
+  name                     = "${module.naming.storage_account.name_unique}-blob"
+  resource_group_name      = azurerm_resource_group.rg_stg.name
+  location                 = azurerm_resource_group.rg_stg.location
   account_tier             = "Standard"
   account_replication_type = "ZRS"
-  shared_access_key_enabled = false
 }
+
